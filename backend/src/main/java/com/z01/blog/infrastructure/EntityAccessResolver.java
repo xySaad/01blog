9599a5fcfc657fc50f770bcef893c04463d9a0c6
xySaad <saadrm.dev@gmail.com>
2026-02-26 23:14:19 +0000

    refactor: streamline EntityAccessResolver and EntityRegistry for improved clarity and performance

--- backend/src/main/java/com/z01/blog/infrastructure/EntityAccessResolver.java
+++ backend/src/main/java/com/z01/blog/infrastructure/EntityAccessResolver.java
@@ -1,20 +1,18 @@
 package com.z01.blog.infrastructure;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.MethodParameter;
 import org.springframework.core.convert.ConversionService;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.HandlerMapping;
 
 import com.z01.blog.annotation.EntityAccess;
@@ -24,10 +22,10 @@ public class EntityAccessResolver<T> implements HandlerMethodArgumentResolver {
     @Autowired
     EntityRegistry entityRegistry;
     @Autowired
-    @Lazy
-    ConversionService conversionService;
-    @Autowired
     PrincipalProvider<T> principalProvider;
+    @Autowired
+    @Lazy
+    private ConversionService conversionService;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -36,35 +34,18 @@ public class EntityAccessResolver<T> implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
-            NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+            NativeWebRequest webRequest, WebDataBinderFactory binderFactory)
+            throws IllegalAccessException, InvocationTargetException {
+        var repoMethod = entityRegistry.getMethodForParam(parameter.getParameter());
 
-        @SuppressWarnings("unchecked")
-        var pathVariables = (Map<String, String>) webRequest
+        Map<String, String> pathVariables = (Map<String, String>) webRequest
                 .getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE, RequestAttributes.SCOPE_REQUEST);
 
-        String entityId = pathVariables.get(parameter.getParameterName());
-        if (entityId == null)
-            throw new MissingPathVariableException(parameter.getParameterName(), parameter);
+        // TODO: support multiple variables
+        String raw = pathVariables.get(parameter.getParameterName());
+        Class<?> idType = repoMethod.method.getParameterTypes()[0];
+        Object convertedId = conversionService.convert(raw, idType);
 
-        var accessInfo = entityRegistry.getAccessInfo(parameter.getParameter());
-
-        Class<?> requiredIdType = accessInfo.method().getParameterTypes()[0];
-
-        Object convertedId = entityId;
-        if (conversionService.canConvert(String.class, requiredIdType)) {
-            convertedId = conversionService.convert(entityId, requiredIdType);
-        }
-
-        Object entity = accessInfo.method().invoke(accessInfo.repoBean(), convertedId);
-        if (entity == null)
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-
-        @SuppressWarnings("unchecked")
-        RestrictedEntity<T> restricted = (RestrictedEntity<T>) entity;
-        restricted.ensureAccess(
-                principalProvider.getCurrentPrincipal(),
-                parameter.getParameterAnnotation(EntityAccess.class).mode());
-
-        return entity;
+        return repoMethod.method.invoke(repoMethod.repo, convertedId);
     }
 }
--- backend/src/main/java/com/z01/blog/infrastructure/EntityRegistry.java
+++ backend/src/main/java/com/z01/blog/infrastructure/EntityRegistry.java
@@ -3,114 +3,94 @@ package com.z01.blog.infrastructure;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
 
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.support.Repositories;
 import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.z01.blog.annotation.AccessMethod;
 import com.z01.blog.annotation.EntityAccess;
 
-record AccessInfo(Object repoBean, Method method) {
+class RepoMethod {
+    public Object repo;
+    public Method method;
+
+    RepoMethod(Object repo) {
+        this.repo = repo;
+        this.method = getAccessMethod(repo);
+    }
+
+    private Method getAccessMethod(Object repo) {
+        for (Method m : repo.getClass().getMethods()) {
+            AccessMethod accessMethod = AnnotationUtils.findAnnotation(m, AccessMethod.class);
+            if (accessMethod != null)
+                return m;
+        }
+
+        throw new RuntimeException("No @AccessMethod found for: " + repo.getClass());
+    }
 }
 
 @Component
-public class EntityRegistry implements InitializingBean {
-
+public class EntityRegistry implements ApplicationListener<ContextRefreshedEvent> {
     @Autowired
     private ApplicationContext context;
-    private final Map<Parameter, AccessInfo> cache = new HashMap<>();
 
-    public AccessInfo getAccessInfo(Parameter param) {
-        return cache.get(param);
-    }
+    private HashMap<Class<?>, RepoMethod> registry = new HashMap<>();
 
     @Override
-    public void afterPropertiesSet() throws Exception {
-        String[] beanNames = context.getBeanNamesForAnnotation(RestController.class);
-        for (String beanName : beanNames) {
-            Class<?> beanType = context.getType(beanName);
-            for (Method method : beanType.getDeclaredMethods()) {
-                if (!AnnotatedElementUtils.hasAnnotation(method, RequestMapping.class))
-                    continue;
+    public void onApplicationEvent(ContextRefreshedEvent event) {
 
-                Parameter[] parameters = method.getParameters();
-
-                for (int i = 0; i < parameters.length; i++) {
-                    Parameter param = parameters[i];
-                    var entityAccess = param.getAnnotation(EntityAccess.class);
-                    if (entityAccess != null) {
-                        Class<?> repoInterface = validateEntityParamter(param, entityAccess);
-                        Object repoBean = context.getBean(repoInterface);
-                        Method repoAccessMethod = findAccessMethod(repoInterface, entityAccess.mode());
-                        validateMethodSignature(repoAccessMethod, repoInterface, param);
-                        cache.put(param, new AccessInfo(repoBean, repoAccessMethod));
-                    }
+        for (String beanName : context.getBeanDefinitionNames()) {
+            Object bean = context.getBean(beanName);
+            for (Method method : bean.getClass().getDeclaredMethods()) {
+                for (Parameter param : method.getParameters()) {
+                    this.registerEntityAccessParam(param);
                 }
             }
         }
     }
 
-    private Class<?> validateEntityParamter(Parameter param, EntityAccess entityAccess) {
-        var ParamType = param.getType();
+    public RepoMethod getMethodForParam(Parameter param) {
+        Class<?> paramType = param.getType();
+        return registry.get(paramType);
+    }
 
-        Assert.isAssignable(
-                RestrictedEntity.class, ParamType,
-                "The provided type does not implement RestrictedEntity");
+    private void registerEntityAccessParam(Parameter param) {
+        EntityAccess entityAccess = param.getAnnotation(EntityAccess.class);
+        if (entityAccess == null)
+            return;
 
-        var repoClass = entityAccess.repo();
-        if (repoClass != Repository.class)
-            return repoClass;
+        Class<?> paramType = param.getType();
+        if (registry.containsKey(paramType))
+            return;
 
+        if (entityAccess.repo() == Repository.class) {
+            var repoMethod = new RepoMethod(findRepo(paramType));
+            registry.put(paramType, repoMethod);
+            return;
+        }
+
+        Class<?> repoClass = entityAccess.repo();
+        Object repoBean = context.getBean(repoClass);
+        var repoMethod = new RepoMethod(repoBean);
+        registry.put(paramType, repoMethod);
+    }
+
+    private Object findRepo(Class<?> paramType) {
         Repositories repositories = new Repositories(context);
-        var repoInfoOpt = repositories.getRepositoryInformationFor(ParamType);
-        if (repoInfoOpt.isEmpty()) {
-            throw new IllegalStateException("No repository for " + ParamType.getName());
-        }
+        Optional<Object> repoOpt = repositories.getRepositoryFor(paramType);
+        if (repoOpt.isEmpty())
+            throw new RuntimeException("No repository found for entity: " + paramType);
 
-        var repoInterface = repoInfoOpt.get().getRepositoryInterface();
-        return repoInterface;
+        Object repo = repoOpt.get();
+        return repo;
     }
 
-    private Method findAccessMethod(Class<?> repoInterface, EntityAccess.Mode mode) {
-        for (Method method : repoInterface.getDeclaredMethods()) {
-            AccessMethod accessMethod = method.getAnnotation(AccessMethod.class);
-            if (accessMethod != null) {
-                return method;
-            }
-        }
-
-        for (Class<?> iface : repoInterface.getInterfaces()) {
-            for (Method method : iface.getDeclaredMethods()) {
-                AccessMethod accessMethod = method.getAnnotation(AccessMethod.class);
-                if (accessMethod != null) {
-                    return method;
-                }
-            }
-        }
-
-        throw new IllegalStateException(
-                "No method annotated with @AccessMethod(" + mode + ") found in repository: " + repoInterface.getName());
-
-    }
-
-    private void validateMethodSignature(Method method, Class<?> repoInterface, Parameter controllerParam) {
-        Class<?> expectedEntityType = controllerParam.getType();
-        Class<?> returnType = method.getReturnType();
-
-        Assert.isAssignable(expectedEntityType, returnType,
-                String.format("Method %s in %s returns %s, but controller expects %s",
-                        method.getName(), repoInterface.getSimpleName(), returnType.getSimpleName(),
-                        expectedEntityType.getSimpleName()));
-
-        Assert.isTrue(method.getParameterCount() == 1, "AccessMethod must take exactly 1 argument (the lookup key).");
-    }
 }
