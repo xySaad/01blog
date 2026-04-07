ce39f77a4792605d5d8ff7bbb17f9f40664ff2da
xySaad <saadrm.dev@gmail.com>
2026-04-07 05:24:14 +0100

    fix: serilize all long fields into json when client doesn't support 64bit integers

--- backend/src/main/java/com/z01/blog/config/JacksonConfig.java
+++ backend/src/main/java/com/z01/blog/config/JacksonConfig.java
@@ -1,26 +1,47 @@
 package com.z01.blog.config;
 
-import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.z01.blog.infrastructure.LongAsStringSerializer;
-
+import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import tools.jackson.core.JsonGenerator;
+import tools.jackson.databind.SerializationContext;
+import tools.jackson.databind.module.SimpleModule;
+import tools.jackson.databind.ser.std.StdSerializer;
+
+class Serializer extends StdSerializer<Long> {
+    Serializer() {
+        super(Long.class);
+    }
+
+    @Override
+    public void serialize(Long value, JsonGenerator gen, SerializationContext context) {
+        var attrs = RequestContextHolder.getRequestAttributes();
+        var request = attrs instanceof ServletRequestAttributes sra ? sra.getRequest() : null;
+
+        if (request != null && "long-as-string".equals(request.getHeader("X-JSON-Format"))) {
+            gen.writeString(value.toString());
+        } else {
+            gen.writeNumber(value);
+        }
+    }
+}
 
 @Configuration
 public class JacksonConfig {
 
-    private final LongAsStringSerializer conditionalLongSerializer;
-
-    public JacksonConfig(LongAsStringSerializer conditionalLongSerializer) {
-        this.conditionalLongSerializer = conditionalLongSerializer;
-    }
-
     @Bean
-    public Module longAsStringModule() {
-        SimpleModule module = new SimpleModule();
-        module.addSerializer(Long.class, conditionalLongSerializer);
-        module.addSerializer(Long.TYPE, conditionalLongSerializer);
-        return module;
+    JsonMapperBuilderCustomizer jacksonCustomizer() {
+        return builder -> {
+            var module = new SimpleModule();
+            var serializer = new Serializer();
+
+            module.addSerializer(Long.class, serializer);
+            module.addSerializer(long.class, serializer);
+
+            builder.addModule(module);
+        };
     }
 }
