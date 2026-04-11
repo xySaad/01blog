8ecf19a26cd6258c83a560d0359126a2404d56aa
xySaad <saadrm.dev@gmail.com>
2026-04-11 21:13:03 +0100

    fix: adjust hidden post access control

--- backend/src/main/java/com/z01/blog/model/Post/PostModel.java
+++ backend/src/main/java/com/z01/blog/model/Post/PostModel.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import com.z01.blog.annotation.EntityAccess.Mode;
 import com.z01.blog.exception.AppError;
 import com.z01.blog.model.BaseEntity;
-import com.z01.blog.model.Account.repo;
 import com.z01.blog.model.Audit.Hideable;
 
 import jakarta.persistence.Column;
@@ -24,12 +23,8 @@ public class PostModel extends BaseEntity implements Hideable {
     @Override
     public void ensureAccess(Long userId, Mode mode) {
         super.ensureAccess(userId, isPublic ? mode : Mode.Write);
-        if (!hidden)
-            return;
-
-        if (account != userId || mode == Mode.Write)
+        if (this.hidden && this.account != userId)
             throw AppError.ACCESS_DENIED.asException();
-
     }
 
     @Override
